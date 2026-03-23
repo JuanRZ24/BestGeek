1b696f44e0306de98cdb84c2307ca4aa05cc7ba3
JuanRZ <juancode24@gmail.com>
2026-03-22 22:44:03 -0700

    eliminacion de importas inutiles

--- inventario-api/src/main/java/com/JRZ/inventario_api/controller/UserController.java
+++ inventario-api/src/main/java/com/JRZ/inventario_api/controller/UserController.java
@@ -8,7 +8,6 @@ import jakarta.validation.Valid;
 import com.JRZ.inventario_api.dto.RegisterRequest;
 import com.JRZ.inventario_api.entity.User;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -20,7 +19,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
 
 import java.util.List;
 import java.util.Map;
